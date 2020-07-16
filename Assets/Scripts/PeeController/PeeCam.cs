32aa3a861adde1e0120f20d758007da1af2193c2
Tony <thepallitta@gmail.com>
2020-07-16 00:11:57 -0400

    Primatives
    +basic scene
    +basic pee

---
+++ Assets/Scripts/PeeController/PeeCam.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeeCam : MonoBehaviour
+{
+    public GameObject peeObject;
+    public float peeSpread = 50f;
+
+    GameObject spawnedPee;
+    private bool firePee;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        firePee = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            if(!IsInvoking("spawnPeeWithForce"))
+                InvokeRepeating("spawnPeeWithForce", 0,0.03f);
+        }
+        else
+        {
+            CancelInvoke("spawnPeeWithForce");
+        }
+    }
+    
+    void spawnPeeWithForce()
+    {
+        
+        Transform t = this.transform;
+        
+        //force vector
+        Vector3 peeVec = 
+            t.position + 
+            t.forward*1000 + 
+            (t.right*Random.Range(-peeSpread, peeSpread)) + 
+            (t.up*Random.Range(-peeSpread, peeSpread)); 
+
+        Vector3 peeOrig = t.position + t.forward*0.5f + t.right; //position of spawn
+
+        spawnedPee = Instantiate(peeObject, peeOrig, Quaternion.identity);
+
+        float mass = spawnedPee.GetComponent<Rigidbody>().mass;
+        spawnedPee.GetComponent<Rigidbody>().AddForce(peeVec*mass);
+    }
+}
---
+++ Assets/Scripts/PeeParticle/AutoKill.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoKill : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    [Tooltip("Seconds before auto-delete.")]
+    public float destroyDelay = 3;
+
+    void Start()
+    {
+        Destroy(gameObject, destroyDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
